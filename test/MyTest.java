0d52debf3ff4bd46ecf43be3a206f1cbfe9cb618
samiksha <samiksha@thoughtworks.com>
2014-01-25 16:00:07 +0530

    first commit

---
+++ test/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by samiksha on 1/25/14.
+ */
+public class MyTest {
+    @Test
+    public  void hello(){
+        assertEquals(1,1);
+    }
+}
